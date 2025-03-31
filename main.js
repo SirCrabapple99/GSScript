1532c874317a5ddaf796486efa5e55f994c3d91f
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-03-31 13:27:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-import * as THREE from 'three';
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(WIDTH, HEIGHT);
+renderer.setClearColor(0xdddddd, 1);
+document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.1, 10000);
+camera.position.z = 50;
+scene.add(camera);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
+const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x0095dd });
+const cube = new THREE.Mesh(boxGeometry, basicMaterial);
+cube.position.x = -25;
+cube.rotation.set(0.4, 0.2, 0);
+scene.add(cube);
 
-camera.position.z = 5;
+const torusGeometry = new THREE.TorusGeometry(7, 1, 16, 32);
+const phongMaterial = new THREE.MeshPhongMaterial({ color: 0xff9500 });
+const torus = new THREE.Mesh(torusGeometry, phongMaterial);
+torus.rotation.set(0.5, 0.5, 0);
+scene.add(torus);
 
-function animate() {
+const strangeGeometry = new THREE.DodecahedronGeometry(7);
+const lambertMaterial = new THREE.MeshLambertMaterial({ color: 0xeaeff2 });
+const dodecahedron = new THREE.Mesh(strangeGeometry, lambertMaterial);
+dodecahedron.position.x = 25;
+scene.add(dodecahedron);
 
-  cube.rotation.x += 0.01;
+const light = new THREE.PointLight(0xffffff);
+light.position.set(-10, 15, 50);
+scene.add(light);
+
+let t = 0;
+function render() {
+  t += 0.01;
+  requestAnimationFrame(render);
   cube.rotation.y += 0.01;
-
-  renderer.render( scene, camera );
-
+  torus.scale.y = Math.abs(Math.sin(t));
+  dodecahedron.position.y = -7 * Math.sin(t * 2);
+  renderer.render(scene, camera);
 }
+render();
