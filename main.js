5351534778a72707c6e216abe1858707f8747c29
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-04-01 08:04:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,40 +8,78 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.1, 10000);
-camera.position.z = 50;
+const camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 10000);
 scene.add(camera);
+camera.position.set(0, 0, 20);
 
 const boxGeometry = new THREE.BoxGeometry(10, 10, 10);
 const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x0095dd });
 const cube = new THREE.Mesh(boxGeometry, basicMaterial);
-cube.position.x = -25;
-cube.rotation.set(0.4, 0.2, 0);
 scene.add(cube);
 
-const torusGeometry = new THREE.TorusGeometry(7, 1, 16, 32);
-const phongMaterial = new THREE.MeshPhongMaterial({ color: 0xff9500 });
-const torus = new THREE.Mesh(torusGeometry, phongMaterial);
-torus.rotation.set(0.5, 0.5, 0);
-scene.add(torus);
+const plane = new THREE.BoxGeometry(1000, 1, 1000);
+const planemat = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const planee = new THREE.Mesh(plane, planemat);
+scene.add(planee);
+planee.position.set(0, -20, 0);
 
-const strangeGeometry = new THREE.DodecahedronGeometry(7);
-const lambertMaterial = new THREE.MeshLambertMaterial({ color: 0xeaeff2 });
-const dodecahedron = new THREE.Mesh(strangeGeometry, lambertMaterial);
-dodecahedron.position.x = 25;
-scene.add(dodecahedron);
 
 const light = new THREE.PointLight(0xffffff);
-light.position.set(-10, 15, 50);
+light.position.set(0, 15, 0);
 scene.add(light);
+
+var keystatus = [0, "KeyW", 0, "KeyA", 0, "KeyS", 0, "KeyD", 0, "Space", 0, "KeyY", 0,];
+
+function keyinput(e) {
+  keystatus[keystatus.indexOf(e.code) - 1] = 1;
+};
+
+function keyupinput(e) {
+  keystatus[keystatus.indexOf(e.code) - 1] = 0;
+};
+
+var camdir = new THREE.Vector3;
+
+function cameramove(e) {
+   camera.rotation.x -= e.movementY / 1000;
+   camera.rotation.y -= e.movementX / 1000;
+}
+
+function mover() {
+  if (keystatus[0] == 1) {
+    camera.getWorldDirection(camdir);
+    camera.position.addScaledVector(camdir, 1);
+  };
+  if (keystatus[2] == 1) {
+    camera.position.x -= 1;
+  };
+  if (keystatus[4] == 1) {
+    camera.getWorldDirection(camdir);
+    camera.position.addScaledVector(camdir, -1);
+  };
+  if (keystatus[6] == 1) {
+  camera.position.x += 1;
+  };
+  if (keystatus[8] == 1) {
+    camera.position.y += 1;
+  };
+  if (keystatus[10] == 1) {
+    camera.position.y -= 1;
+  };
+}
+document.addEventListener("click", function(){
+  document.body.requestPointerLock();
+});
+document.addEventListener("mousemove", cameramove);
+
+document.addEventListener("keypress", keyinput);
+document.addEventListener("keyup", keyupinput);
 
 let t = 0;
 function render() {
+  mover();
   t += 0.01;
   requestAnimationFrame(render);
-  cube.rotation.y += 0.01;
-  torus.scale.y = Math.abs(Math.sin(t));
-  dodecahedron.position.y = -7 * Math.sin(t * 2);
   renderer.render(scene, camera);
 }
 render();
