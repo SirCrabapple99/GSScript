39e1866a83fbab52595254b88a152a5d4ec5148b
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2025-04-29 09:38:53 -0500

    Update ui.js

--- js/levelmaker/ui.js
+++ js/levelmaker/ui.js
@@ -1,1 +1,12 @@
-
+document.getElementById("shapes").addEventListener('click', shapesdown);
+function shapesdown(e) {
+    if (document.getElementById('shapesdiv').childNodes[0].classList == 'shapes shapesdown ubuntu-light') {
+        document.getElementById('shapesdiv').childNodes[0].classList = 'shapes ubuntu-light'
+        document.getElementById('shapesdiv').childNodes[2].classList = 'nbox ubuntu-light'
+        document.getElementById('shapesdiv').childNodes[2].disabled = 'true'
+    } else {
+        document.getElementById('shapesdiv').childNodes[0].classList = 'shapes shapesdown ubuntu-light'
+        document.getElementById('shapesdiv').childNodes[2].classList = 'nbox shapesdown1 ubuntu-light'
+        document.getElementById('shapesdiv').childNodes[2].disabled = 'false'
+    }
+}
